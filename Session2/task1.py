76118735381cd634f0bcd87e81a48dc30765c742
Gounderpriyanka <gounderpriyanka4@gmail.com>
2026-06-29 15:48:43 +0530

    ....

--- Session2/task1.py
+++ Session2/task1.py
@@ -1,3 +1,10 @@
+"""
+Task 1.Create three variables in Python named item_price, delivery_fee, and coupon_discount, 
+and assign them values representing a Swiggy order (e.g., 250, 30, 20). 
+Print each variable.
+"""
+
+
 item_price = 250
 delivery_fee = 30
 coupon_discount = 20
--- Session2/task3.py
+++ Session2/task3.py
@@ -1,4 +1,10 @@
 """
+Task 3.Add both single-line and multi-line comments in your script explaining
+ what each section does, using # for single-line and triple quotes for multi-line comments.
+
+"""
+
+"""
 This program checks whether a product
 is eligible for free delivery.
 """
