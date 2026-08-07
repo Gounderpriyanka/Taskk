9a7bcef97ed4631e2459a9deb5a460d38c426e0c
Gounderpriyanka <gounderpriyanka4@gmail.com>
2026-08-07 16:33:00 +0530

    .......

--- Session14/Tasks.py
+++ Session14/Tasks.py
@@ -13,10 +13,10 @@
 #2.Read the playlist.txt file you created and display each song name
 #  in uppercase letters using Python.
 
-with open("playlist.txt",'r') as f:
+'''with open("playlist.txt",'r') as f:
     content = f.readlines()
     for i in content:
-        print(i.upper())
+        print(i.upper())'''
 
 #3.Download a sample CSV file of IPL match scores (you can create your own with
 #columns: match_id, team1, team2, winner) and write a Python script to read the file
@@ -24,21 +24,52 @@ with open("playlist.txt",'r') as f:
 
 import csv as cv
 
-with open("IPL_Match_S(core.csv",'w') as file:
+'''with open("IPL_Match_Score.csv",'w',newline='') as file:
     writer = cv.writer(file)
     writer.writerow(['match_id','team1','team2','winner'])
     writer.writerow(["101","Chennai Super Kings","Mumbai Indians","Chennai Super Kings"])
     writer.writerow(["102","Royal Challengers Bengaluru","Kolkata Knight Riders","Kolkata Knight Riders"])
     writer.writerow(["103","Gujarat Titans","Rajasthan Royals","Gujarat Titans"])
     writer.writerow(["104","Sunrisers Hyderabad","Punjab Kings","Sunrisers Hyderabad"])
-    writer.writerow(["105","Delhi Capitals","Lucknow Super Giants","Lucknow Super Giants"])
+    writer.writerow(["105","Delhi Capitals","Lucknow Super Giants","Lucknow Super Giants"])'''
+
+'''with open("IPL_Match_Score.csv",'r') as file:
+    reader = cv.reader(file)
+    for i in reader:
+        print(i[3::])
 
     file.close()
-
- 
- 
- 
- 
 '''
- 4.Find a public JSON file of trending movies (or create your own movies.json with at least 3 movie objects containing title, year, and rating), then use the json module in Python to load the file and print the title and rating of each movie.
-5.Use the pathlib module to check if a file called 'my_fav_apps.json' exists in your current directory, and if not, create it and write a JSON array of your top 3 mobile apps (e.g., Instagram, Zomato, Paytm) with their names and categories.<br><br><em><strong>Hint:</strong> Use Path('my_fav_apps.json').exists() to check for the file, and json.dump() to write the data.</em>'''
+ 
+# 4.Find a public JSON file of trending movies (or create your own movies.json with at least 3 movie objects 
+# containing title, year, and rating), then use the json module in Python to load the file and
+#  print the title and rating of each movie.
+
+import json
+
+with open("movies.json",'w') as file:
+    data =[
+        {
+        "title": "Inception",
+        "year": 2010,
+        "rating": 8.8
+        },
+            {
+        "title": "Interstellar",
+        "year": 2014,
+        "rating": 8.7
+    },
+    {
+        "title": "The Dark Knight",
+        "year": 2008,
+        "rating": 9.0
+    }
+    ]
+    file.write(json.dumps(data,indent=3))
+    file.close()
+
+
+
+
+'''
+ 5.Use the pathlib module to check if a file called 'my_fav_apps.json' exists in your current directory, and if not, create it and write a JSON array of your top 3 mobile apps (e.g., Instagram, Zomato, Paytm) with their names and categories.<br><br><em><strong>Hint:</strong> Use Path('my_fav_apps.json').exists() to check for the file, and json.dump() to write the data.</em>'''
