cea9c99efa1c69edfabaaa0950a500704ddfbd35
Gounderpriyanka <gounderpriyanka4@gmail.com>
2026-08-11 23:48:26 +0530

    ...

--- Extra_ModuleQuestions/Task.py
+++ Extra_ModuleQuestions/Task.py
@@ -7,7 +7,22 @@ print("the sum of numbers:",summ)'''
 
 #2. Write a Python program to count occurrences of a substring in a string.
 
-n = input("Enter a word:")
+'''n = input("Enter a word:")
+sub_string = input("Enter your substring:")
+count = n.count(sub_string)
+print(f"The Substring is appeared {count} times")'''
 
-for i in n:
-       
+#3. Write a Python program to count the occurrences of each word in a given sentence.
+
+'''word = "python is easy and python is powerful"
+split = word.split(" ")
+
+for i in split:
+    count = word.count(i)
+    print(f"'{i}' : {count}")'''
+
+#4. Write a Python program to get a single string from two given strings, separated by a space and
+#  swap the first two characters of each string.
+
+
+
